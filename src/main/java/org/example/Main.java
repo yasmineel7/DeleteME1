7a5cb31b176ebc801a306ea6d74221023ebfcc79
Yasmine El Hattab <6298686@edu.vaniercollege.qc.ca>
2025-04-29 14:41:30 -0400

    Add bubblesort in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,50 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     int[] nums = {3, 1, 5, 2, 1, 4};
+     bubbleSort(nums); //{1,1,2,3,4,5}
+        System.out.println(Arrays.toString(nums));
+        int num1 = 1;
+        int num2 = 2;
+
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    /*
+    31  5214
+    1 35 214
+    13 52 14
+    132 51 4
+    1321 54
+    132145     | 5
+
+    13 214
+    1 32 14
+    12 31 4
+    121 34
+    12134   | 4
+
+    12 13
+    1 21 3
+    11 23 | 3
+
+    112
+     */
+    public static void bubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int k = 0; k < nums.length - 1 - i; k++) {
+                if (nums[k] > nums[k +1]) {
+                    //swap
+                    int temp = nums[k];
+                    nums[k] = nums[k + 1];
+                    nums[k + 1] = temp; 
+                }
+            }
+        }
     }
 }
